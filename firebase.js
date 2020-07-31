248567bdbb846fe924586e5ffc71ca4d2b0403b2
Jay Patel <jay.p1@ahduni.edu.in>
2020-07-31 21:20:38 +0530

    First

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDrINjTmhXr7K7kVlh6BqgsveyvqfLLqic",
+    authDomain: "tiktok-clone-faba6.firebaseapp.com",
+    databaseURL: "https://tiktok-clone-faba6.firebaseio.com",
+    projectId: "tiktok-clone-faba6",
+    storageBucket: "tiktok-clone-faba6.appspot.com",
+    messagingSenderId: "918485517993",
+    appId: "1:918485517993:web:2a61fbedfee672e10c8bb8",
+    measurementId: "G-Q87991N8MM"
+};
