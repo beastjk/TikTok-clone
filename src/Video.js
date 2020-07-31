70cda5300f8bc9d33876ed8053c844ded6dfea1b
Jay Patel <jay.p1@ahduni.edu.in>
2020-07-31 21:54:07 +0530

    Video Add

--- src/Video.js
+++ src/Video.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './Video.css'
+import VideoFooter from './VideoFooter'
 
 function Video() {
+    const [play, setPlay] = useState(false)
+    const videoRef = useRef(null)
+    const onVideoPress = () => {
+        if(play){
+            videoRef.current.pause()
+            setPlay(false)
+        }
+        else{
+            videoRef.current.play()
+            setPlay(true)
+        }
+        
+    }
     return (
         <div className = 'video'>
-            <h1>This is a video</h1>
+            <video className = "video__player" loop ref = {videoRef} onClick = {onVideoPress} src="http://techslides.com/demos/sample-videos/small.mp4"></video>
+            <VideoFooter />
         </div>
     )
 }
---
+++ src/VideoFooter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './VideoFooter.css'
+
+function VideoFooter() {
+    return (
+        <div className = "videoFooter">
+            Footer
+        </div>
+    )
+}
+
+export default VideoFooter
